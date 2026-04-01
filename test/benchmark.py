50b8fe9969a2dfbc8500ade693dc3f24b3138629
Chocolate <artemnes1j@gmail.com>
2026-04-01 19:45:20 +0300

    Add time benchmarking python

--- test/benchmark.py
+++ test/benchmark.py
@@ -1,10 +1,13 @@
 import json
 import sys
+from x86_build import build_exec, BENCHMARKS_BUILD_DIR
+import time
+import subprocess
 
 data = [
     {
         "name": "test1",
-        "metric1": "val11",
+        "--enable-dce": "val11",
         "metric2": "val12"
     }, {
         "name": "test1",
@@ -17,8 +20,39 @@ data = [
     }
 ]
 
-BenchmarkingTests = [
-        "Fibbonacci"
+data = []
+
+benchmarking_tests = [
+        "FibbonacciRecursiveMulltiple"
     ]
+
+if len(sys.argv) != 2:
+    raise RuntimeError("Unexpected number of parameters")
+
+compiler_path = sys.argv[1]
+workdir       = sys.argv[2]
+
+def run_single_test(test: str, sccp: bool, dce: bool):
+    exec = build_exec(compiler_path, workdir, test, enable_sccp=sccp, enable_dce=dce)
+    json_output = BENCHMARKS_BUILD_DIR + "/" + test + f".sccp_{sccp}.dce_{dce}result.json"
+    subprocess.run(["hyperfine", "--export-json", json_output])
+    with open(json_output, "r") as f:
+        data = f.read()
+        json_data = json.loads(data)
+
+    print(json_data)
+    data.append(
+        {
+            "name": test,
+            "--(none)": 0,
+            "--enable-sccp": 0,
+            "--enable-sccp --enable-dce": 0
+        }
+    )
+
+for test in benchmarking_tests:
+    run_single_test(test, sccp=False, dce=False)
+    run_single_test(test, sccp=True,  dce=False)
+    run_single_test(test, sccp=True,  dce=True)
 
 json.dump(data, sys.stdout)
