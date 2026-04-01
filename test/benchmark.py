926cfe6d6555bfbc2eac329839a2944dbdd1b577
Chocolate <artemnes1j@gmail.com>
2026-04-01 08:19:26 +0300

    Fix table

--- test/benchmark.py
+++ test/benchmark.py
@@ -1,11 +1,20 @@
 import json
 import sys
 
-data = {
-    "columns": ["name", "metric1", "metric2"],
-    "name": ["test1", "test2", "test3"],
-    "metric1": ["val11", "val12", "val13"],
-    "metric2": ["val21", "val22", "val23"]
-}
+data = [
+    {
+        "name": "test1",
+        "metric1": "val11",
+        "metric2": "val12"
+    }, {
+        "name": "test1",
+        "metric1": "val11",
+        "metric2": "val12"
+    }, {
+        "name": "test1",
+        "metric1": "val11",
+        "metric2": "val12"
+    }
+]
 
 json.dump(data, sys.stdout)
