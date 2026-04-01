bd2677a9b2fcb395f6a0a1721b811c7e6eecf4ce
Chocolate <artemnes1j@gmail.com>
2026-04-01 07:53:12 +0300

    Fix table

--- test/benchmark.py
+++ test/benchmark.py
@@ -1,6 +1,7 @@
 import json
 
 data = {
+    "columns": ["name", "metric1", "metric2"],
     "name": ["test1", "test2", "test3"],
     "metric1": ["val11", "val12", "val13"],
     "metric2": ["val21", "val22", "val23"]
